97cffc195afc0824fb9fc71b5fd2355e4519fa73
Awais <awaismasood508@gmail.com>
2025-12-19 19:22:31 +0500

    String Sclicing

--- Learning.py
+++ Learning.py
@@ -5,3 +5,22 @@ systems.reverse()
 
 print('Updated List:', systems)
 print ('hello')
+a ="asdfghjklqwertuiop"
+#print(a[0;2;10])q
+
+#A list of numbers from 0 to 90
+my_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+# Extracting a slice
+# Start at index 1, stop before index 8, take every 2nd item
+subset = my_list[1:8:2]
+
+print(subset)
+alphabet = "abcdefghij"
+
+# Start at index 1 ('b')
+# Stop before index 8 ('i')
+# Step by 2 (skip every other character)
+substring = alphabet[1:8:3]
+
+print(substring)
